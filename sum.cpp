ba9cc0ae9aa7683ae8d5b5f51d20244c32022c23
Yogesh <yogeshvaswani02@gmail.com>
2023-07-04 21:37:17 +0530

    first

---
+++ sum.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    cout<<"hello sum"<<endl;
+ return 0; 
+}
---
+++ sum2.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    cout<<"hellosum2"<<endl;
+ return 0; 
+}
